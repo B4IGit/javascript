eb8e306d3277fe9d4f1efbc28ff0d0451a8221ad
B4IGit <dfledesma@dmacc.edu>
2024-07-24 14:11:44 -0500

    Canvas-sketch. Final animation.

--- sketches/sketch-03.js
+++ sketches/sketch-03.js
@@ -1,16 +1,23 @@
 const canvasSketch = require('canvas-sketch');
 const random = require('canvas-sketch-util/random');
+const math = require('canvas-sketch-util/math');
 
 const settings = {
   dimensions: [ 1080, 1080 ],
   animate: true
 };
 
+const animate = () => {
+  console.log('Devin');
+  requestAnimationFrame(animate);
+};
+// animate();
+
 const sketch = ({ context, width, height }) => {
 
   const agents = [];
 
-  for (let i = 0; i < 300; i++) {
+  for (let i = 0; i < 40; i++) {
     const x = random.range(0, width);
     const y = random.range(0, height);
 
@@ -22,9 +29,29 @@ const sketch = ({ context, width, height }) => {
     context.fillStyle = 'white';
     context.fillRect(0, 0, width, height);
 
+    for (let i = 0; i < agents.length; i++) {
+      const agent = agents[i];
+
+      for (let j = i + 1; j < agents.length; j++) {
+        const other = agents[j];
+
+        const dist = agent.pos.getDistance(other.pos);
+
+        if (dist > 200) continue;
+
+        context.lineWidth = math.mapRange(dist, 0, 200, 12, 1);
+
+        context.beginPath();
+        context.moveTo(agent.pos.x, agent.pos.y);
+        context.lineTo(other.pos.x, other.pos.y);
+        context.stroke();
+      }
+    }
+
     agents.forEach(agent => {
       agent.update();
       agent.draw(context);
+      agent.wrap(width, height);
     })
   };
 };
@@ -37,6 +64,13 @@ class Vector {
     this.x = x;
     this.y = y;
   }
+
+  // pythagorean theorem
+  getDistance(v) {
+    const dx = this.x - v.x;
+    const dy = this.y - v.y;
+    return Math.sqrt(dx * dx + dy * dy);
+  }
 }
 
 class Agent {
@@ -44,6 +78,17 @@ class Agent {
     this.pos = new Vector(x, y);
     this.vel = new Vector(random.range(-1, 1), random.range(-1, 1));
     this.radius = random.range(4, 12);
+  }
+
+ bounce(width, height) {
+    if (this.pos.x <= 0 || this.pos.x >= width) this.vel.x *= -1;
+    if (this.pos.y <= 0 || this.pos.y >= height) this.vel.y *= -1;
+  }
+
+
+  wrap(width, height){
+    if (this.pos.x <= 0 || this.pos.x >= width) this.pos.x = width -this.pos.x;
+    if (this.pos.y <= 0 || this.pos.y >= height) this.pos.y = height - this.pos.y;
   }
 
   update() {
@@ -55,10 +100,10 @@ class Agent {
     context.save();
     context.translate(this.pos.x, this.pos.y);
 
-    context.lineWidth = 4;
+    context.lineWidth = 3;
 
     context.beginPath();
-    context.arc(this.pos.x, this.pos.y, this.radius, 0, Math.PI * 2);
+    context.arc(0, 0, this.radius, 0, Math.PI * 2);
     context.fill();
     context.stroke();
 
